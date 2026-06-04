77e325f08c9a733e20d2c2d74b9defd3189af7a2
fitim <fitim@DESKTOP-KJR4TR2>
2026-06-04 18:04:36 +0200

    feat: show version in sidebar footer

--- AiCV.Web/Components/Layout/NavMenu.razor.cs
+++ AiCV.Web/Components/Layout/NavMenu.razor.cs
@@ -5,6 +5,14 @@ public partial class NavMenu
     [Parameter]
     public bool ShowLoggedInAs { get; set; } = true;
 
+    [Parameter]
+    public bool IsExpanded { get; set; } = true;
+
+    private string AppVersion { get; } = AppVersionProvider.GetDisplayVersion();
+
+    private string NavMenuShellClass =>
+        IsExpanded ? "nav-menu-shell" : "nav-menu-shell nav-menu-shell-collapsed";
+
     private async Task Logout()
     {
         await JSRuntime.InvokeVoidAsync(
